ea8fbc0379fd70d8d2f76f6ae4983a19d023058a
LEHUYDEV <52237819+LEHUYDEV@users.noreply.github.com>
2019-07-02 14:51:26 +0700

    Add files via upload

---
+++ bai 5.py
@@ -0,0 +1,8 @@
+inventory = {
+    'wold': 500,
+    'pouch': ['flint','twine','gemstone'],
+    'backpack':[ 'xylophone','dagger','bedroll','bread loaf']
+}
+#Add a key to inventory called 'pocket'.
+inventory['tui']= []
+print(inventory)
